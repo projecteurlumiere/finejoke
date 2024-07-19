80ec72289d7ca29644c89755105ef0674fa42901
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-07-19 19:26:12 +0300

    fix selecting many rooms in lobby

--- app/javascript/controllers/catalogue_controller.js
+++ app/javascript/controllers/catalogue_controller.js
@@ -2,25 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="catalogue"
 export default class extends Controller {
-  static targets = [ "connect", "view" ]
+  static targets = [ "connect", "view", "entry" ]
 
   select(e) {
     if (e.target.classList.contains("select")) { this.#deselect(e); return }
+    this.#deselectTheRest();
 
     this.connectTarget.href = e.target.dataset.joinPath;
     this.viewTarget.href = e.target.dataset.viewPath;
-    this.#removeDisabled();
-
     e.target.classList.add("select")
+
+    this.#removeDisabled();
   }
 
   #deselect(e) {
     this.#addDisabled();
+    e.target.classList.remove("select")
 
     this.connectTarget.href = "";
     this.viewTarget.href = "";
+  }
 
-    e.target.classList.remove("select")
+  #deselectTheRest() {
+    for (var i = this.entryTargets.length - 1; i >= 0; i--) {
+      this.entryTargets[i].classList.remove("select")
+    }
   }
 
   #removeDisabled() {
