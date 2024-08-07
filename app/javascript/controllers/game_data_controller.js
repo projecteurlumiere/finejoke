0b6b4b20cd08c9f236cbf47f590573aae0972785
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-08-07 17:33:57 +0300

    style game user list

---
+++ app/javascript/controllers/game_data_controller.js
@@ -0,0 +1,57 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="game-list"
+export default class extends Controller {
+  static values = {
+    id: String,
+    host: Boolean
+  }
+
+  static targets = [ "user" ]
+
+  userTargetConnected(node) {
+    if (node.dataset.id == this.idValue) {
+      this.hostValue = node.dataset.host
+    } else if (this.hostValue == true) {
+      this.#hostButtons("show", node)
+    }     
+  }
+
+  userTargetDisconnected(node) {
+    if (node.dataset.id == this.idValue) this.hostValue = false 
+  }
+
+  #hostValueChanged() {
+    const directive = this.hostValue === true ? "show" : "false"
+
+    for (var i = this.userTargets.length - 1; i >= 0; i--) {
+      
+      this.#hostButtons(directive, this.userTargets[i])
+    }
+  }
+
+  #hostButtons(directive, node) { 
+    const hiddenNodes = node.querySelectorAll(".settings .hidden");
+
+    for (var i = hiddenNodes.length - 1; i >= 0; i--) {
+      const list = hiddenNodes[i].classList
+
+      if (hiddenNodes[i].dataset.id == this.idValue) {
+        list.add("hidden")
+        return
+      }
+
+      if (directive === "hide") { list.add("hidden") } 
+      if (directive === "show") { list.remove("hidden") }
+    }
+  }
+
+  showSettings(e) {
+    e.target.parentNode.classList.add("clicked");
+
+    setTimeout(() => {
+      e.target.parentNode.classList.remove("clicked");
+    }, "2000");
+
+  }
+}
--- app/policies/game_policy.rb
+++ app/policies/game_policy.rb
@@ -20,7 +20,7 @@ class GamePolicy < ApplicationPolicy
   end
 
   def destroy?
-    @user&.host? && @user.playing(@game)
+    @user&.host? && @user.playing?(@game)
   end
 
   def join?
