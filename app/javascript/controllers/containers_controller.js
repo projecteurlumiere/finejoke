1b83cd98cc6b8e00e173f79b96a2bea226b36f32
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-08-08 15:07:01 +0300

    add mobile layout for game page & refactor containers controller

---
+++ app/javascript/controllers/containers_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="containers"
+export default class extends Controller {
+  static targets = [ "container", "button"]
+
+  show(e) {
+    const name = e.target.dataset.containerName;
+
+    console.log(name)
+    this.#showContainer(name);
+    this.#highlightButton(e.target);
+  }
+
+  #showContainer(name) {
+    for (var i = this.containerTargets.length - 1; i >= 0; i--) {
+      const classList = this.containerTargets[i].classList
+      console.log(this.containerTargets[i].dataset.containerName)
+
+      if (this.containerTargets[i].dataset.containerName == name) {
+        classList.remove("hidden-when-mobile")
+      } 
+      else {
+        classList.add("hidden-when-mobile")
+      }
+    }
+  }
+
+  #highlightButton(node) {
+    for (var i = this.buttonTargets.length - 1; i >= 0; i--) {
+      const classList = this.buttonTargets[i].classList
+      console.log(this.buttonTargets[i])
+
+
+      if (this.buttonTargets[i] != node) {
+        classList.remove("selected")
+      } else {
+        classList.add("selected")
+      }
+    }
+  }
+}
