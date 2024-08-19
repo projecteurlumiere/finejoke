4dfb80d6313a3e22aed06c05375c9de724a67b9e
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-08-19 16:58:06 +0300

    implement notifications for containers & custom ones for chat

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -13,38 +13,56 @@ export default class extends Controller {
     const chatHistory = this.#getChatHistory(this.key);
 
     this.messagesTarget.innerHTML = chatHistory.HTML
-    console.log(this.messageTargets.length)
+    this.#removeExcess();
+    this.scrollObserver = this.#createScrollObserver();
+    for (var i = this.messageTargets.length - 1; i >= 0; i--) {
+      this.messageTargets[i].classList.remove("unseen")
+      this.scrollObserver.observe(this.messageTargets[i])
+    }
 
-    this.#scrollToBottom();
-    this.observer = this.#createObserver();
-    if (this.messageTargets.slice(-1)[0]) this.observer.observe(this.messageTargets.slice(-1)[0])
+    this.mustScrollToBottom = true
+    setTimeout(() => { this.#scrollToBottom(); }, 100)
     this.#clearChatHistories();
   }
 
   resetForm(e) {
     if (e.target != this.formTarget) return
-    if (e.detail.success) this.formTarget.reset();
+    if (e.detail.success) {
+      this.#scrollToBottom();
+      this.formTarget.reset();
+    }
   }
 
   messageTargetConnected(element) {
-    if (this.messageTargets.slice(-1)[0] != element) return
+    if (this.#lastMessage() != element) return
 
-    if (this.messageTargets.length > 100) {
-      this.messageTargets[0].remove()
-    }
+    this.#removeExcess();
 
     const penultimateMessage = this.messageTargets.slice(-2, -1)[0]
 
-    if (penultimateMessage) {
-      this.observer.unobserve(penultimateMessage)
-      if (penultimateMessage.classList.contains("currently-viewed")) {
-        penultimateMessage.classList.remove("currently-viewed")
-        this.#scrollToBottom();
-      }
+    if (!penultimateMessage || penultimateMessage.classList.contains("currently-viewed")) {
+      this.#scrollToBottom();
     }
-    this.observer.observe(element)
+
+    this.scrollObserver.observe(element);
+
+    setTimeout(() => {
+      if (this.#lastMessage().classList.contains("unseen")) { 
+        this.dispatch("unseen", { detail: { containerName: this.element.dataset.containerName } })
+      }
+    }, 100)
 
     this.#storeChatHistory()
+  }
+
+  adjustScrollOnResize(e){
+    if (this.#lastMessage().classList.contains("currently-viewed")) {
+      this.#scrollToBottom();
+    }
+  }
+
+  #lastMessage() {
+    return this.messageTargets.slice(-1)[0]
   }
 
   #clearChatHistories(){
@@ -62,8 +80,25 @@ export default class extends Controller {
     }
   }
 
+  #removeExcess(){
+    if (this.messageTargets.length > 100) {
+      const oldMessages = this.messageTargets.slice(0, -100)
+
+      for (var i = oldMessages.length - 1; i >= 0; i--) {
+        if (this.scrollObserver) {
+          this.scrollObserver.unobserve(oldMessages[i])
+        }
+
+        oldMessages[i].remove()
+      }
+    }
+  }
+
   #scrollToBottom(){
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
+    for (var i = this.messageTargets.length - 1; i >= 0; i--) {
+      this.messageTargets[i].classList.remove("unseen")
+    }
   }
 
   #storeChatHistory() {
@@ -91,15 +126,29 @@ export default class extends Controller {
     }
   }
 
-  #createObserver(element) {
+  #createScrollObserver() {
     return new IntersectionObserver((elements) => {  
+      const lastMessage = this.#lastMessage()
+
       for (var i = elements.length - 1; i >= 0; i--) {
         if (elements[i].isIntersecting) {
           elements[i].target.classList.add("currently-viewed");
+          elements[i].target.classList.remove("unseen");
+          if (elements[i].target === lastMessage) {
+            this.dispatch("seen", { detail: { containerName: this.element.dataset.containerName } })
+          }
         } 
         else {
+          if (this.element.classList.contains("hidden-when-mobile") && elements[i] === lastMessage) {
+            this.mustScrollToBottom = true
+          }
           elements[i].target.classList.remove("currently-viewed");
         }
+      }
+
+      if (!this.element.classList.contains("hidden-when-mobile") && this.mustScrollToBottom) { 
+        this.mustScrollToBottom = false;
+        this.#scrollToBottom();
       }
     }, {
       root: this.messagesTarget,
--- app/javascript/controllers/containers_controller.js
+++ app/javascript/controllers/containers_controller.js
@@ -4,8 +4,22 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "container", "button"]
 
+  connect() {
+    for (var i = this.buttonTargets.length - 1; i >= 0; i--) {
+      this.buttonTargets[i].classList.remove("unseen")
+    }
+  }
+
   show(e) {
     this.#showByName(e.currentTarget.dataset.containerName);
+  }
+
+  blink(e){
+    this.#blinkButton(e.detail.containerName)
+  }
+
+  unblink(e) {
+    this.#unblinkButton(e.detail.containerName)
   }
 
   #showByName(name) {
@@ -18,6 +32,7 @@ export default class extends Controller {
 
     for (var i = this.containerTargets.length - 1; i >= 0; i--) {
       if (!this.containerTargets[i].classList.contains("hidden-when-mobile") && this.containerTargets[i] != element) {
+        this.#blinkButton(name);
         this.#showByName(this.containerTargets[i].dataset.containerName)
         return
       }
@@ -26,13 +41,14 @@ export default class extends Controller {
     this.#showByName(name)
   }
 
-
   #showContainer(name) {
     for (var i = this.containerTargets.length - 1; i >= 0; i--) {
       const classList = this.containerTargets[i].classList
 
       if (this.containerTargets[i].dataset.containerName == name) {
         classList.remove("hidden-when-mobile")
+        if (this.containerTargets[i].dataset.customUnblink === "true") continue
+        this.#unblinkButton(name)
       } 
       else {
         classList.add("hidden-when-mobile")
@@ -48,6 +64,22 @@ export default class extends Controller {
         classList.remove("selected")
       } else {
         classList.add("selected")
+      }
+    }
+  }
+
+  #blinkButton(name) {
+     for (var i = this.buttonTargets.length - 1; i >= 0; i--) {
+      if (this.buttonTargets[i].dataset.containerName === name) {
+        this.buttonTargets[i].classList.add("unseen")
+      }
+    }
+  }
+
+  #unblinkButton(name) {
+    for (var i = this.buttonTargets.length - 1; i >= 0; i--) {
+      if (this.buttonTargets[i].dataset.containerName === name) {
+        this.buttonTargets[i].classList.remove("unseen")
       }
     }
   }
