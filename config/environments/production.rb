9591d13cd5edaa8471d516226e37be71dc7b1d8f
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-08-19 12:48:13 +0300

    configure js compression for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  config.assets.js_compressor = :terser
+
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
