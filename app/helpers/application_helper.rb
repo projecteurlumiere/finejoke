9bea36c1f0705f876768977ca4da647ddee67dbe
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-08-28 15:05:57 +0300

    add fonts preloading for production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       raw file.read
     end
   end
+
+  def preload_fonts
+    Dir.entries("app/assets/fonts").map do |font_name|
+      next unless font_name.end_with?(".woff2")
+
+      preload_link_tag font_name, crossorigin: ""
+    end.join(" ").html_safe
+  end
 end
