2ed964045e4d590e013914aa73066a354503f86d
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-09-13 14:30:18 +0300

    refactor suggestion controller js

--- app/javascript/controllers/suggestion_controller.js
+++ app/javascript/controllers/suggestion_controller.js
@@ -10,8 +10,10 @@ export default class extends Controller {
     "response"
   ]
 
-  connect() {
+  copy() {
+    if (!this.hasUserInputTarget) return
 
+    this.#cloneInput();
   }
 
   inputTargetConnected() {
@@ -19,17 +21,19 @@ export default class extends Controller {
   }
 
   async responseTargetConnected(el) {
-    this.#toggleInput("disable")
+    const suggestion = el.innerText
+    el.remove()
 
+    this.#toggleInput("disable")
     if (this.typed) this.typed.destroy()
 
-    const placeholder = this.injectPlaceholder(this.userInputTarget)
-    this.userInputTarget.value = el.innerText
+    const placeholder = this.#injectPlaceholder(this.userInputTarget)
+    this.userInputTarget.value = suggestion
 
-    const typeSpeed = 15
+    const typeSpeed = 15 // ms
 
     this.typed = new Typed(placeholder, {
-      strings: [el.innerText],
+      strings: [suggestion],
       typeSpeed: typeSpeed,
       showCursor: false,
       loop: false,
@@ -37,24 +41,52 @@ export default class extends Controller {
         this.typed.destroy() 
       },
       onDestroy: () => {
-        // this.userInputTarget.value = el.innerText
-        this.userInputTarget.style.visibility = ""
-        this.userInputTarget.scrollTop = this.userInputTarget.scrollHeight;
-
-        this.#toggleInput("enable")
-
-        // scroll the input once
-        this.userInputTarget.dispatchEvent(new Event("input"))
-        el.remove()
-        placeholder.remove()
-
+        this.#removePlaceholder(placeholder);
         this.typed = undefined
       }
     });
-    
+
+    this.#scrollPlaceholder(placeholder, typeSpeed, suggestion)
+  }
+
+  
+  #toggleInput(directive) {
+    [this.userInputTarget, this.userSubmitTarget, this.suggestionSubmitTarget].forEach((el) => {
+      el.disabled = directive === "disable" ? true : false
+    })
+  }
+
+  #cloneInput() {
+    this.suggestionFormInputTarget.value = this.userInputTarget.value
+  }
+
+  #injectPlaceholder(textarea) {
+    const placeholder = document.createElement("div")
+
+    placeholder.classList.add("textarea")
+    placeholder.dataset.action = "input->symbol-counter#update"
+    placeholder.innerHTML = textarea.value
+
+    this.userInputTarget.insertAdjacentElement("afterend", placeholder)
+    this.userInputTarget.style.visibility = "hidden"
+
+    return placeholder
+  }
+
+  #removePlaceholder(placeholder) {
+    this.userInputTarget.style.visibility = ""
+    this.userInputTarget.scrollTop = this.userInputTarget.scrollHeight;
+    this.#toggleInput("enable")
+    // scroll the input once
+    this.userInputTarget.dispatchEvent(new Event("input"))
+    placeholder.remove()
+  }
+
+  async #scrollPlaceholder(placeholder, typeSpeed, suggestion) {
     // if something goes wrong it will turn off
-    const timeLimit = (typeSpeed * el.innerText.length * 2)
+    const timeLimit = typeSpeed * suggestion.length * 2
     let timePassed = 0;
+
     while (this.typed || timePassed > timeLimit) {
       // scrolling bottom
       placeholder.scrollTop = placeholder.scrollHeight
@@ -69,34 +101,5 @@ export default class extends Controller {
       await new Promise(r => setTimeout(r, typeSpeed));
       timePassed += typeSpeed
     }
-  }
-
-  injectPlaceholder(textarea) {
-    const placeholder = document.createElement("div")
-
-    placeholder.classList.add("textarea")
-    placeholder.dataset.action = "input->symbol-counter#update"
-    placeholder.innerHTML = textarea.value
-
-    this.userInputTarget.insertAdjacentElement("afterend", placeholder)
-    this.userInputTarget.style.visibility = "hidden"
-
-    return placeholder
-  }
-
-  copy() {
-    if (!this.hasUserInputTarget) return
-
-    this.#cloneInput();
-  }
-
-  #cloneInput() {
-    this.suggestionFormInputTarget.value = this.userInputTarget.value
-  }
-
-  #toggleInput(directive) {
-    [this.userInputTarget, this.userSubmitTarget, this.suggestionSubmitTarget].forEach((el) => {
-      el.disabled = directive === "disable" ? true : false
-    })
   }
 }
