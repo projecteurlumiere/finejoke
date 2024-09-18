c955f6dd75b8c457501bae5e60bd0543aefc9af2
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-09-18 18:49:02 +0300

    fix zeitwerk error

--- app/models/concerns/sanitizable.rb
+++ app/models/concerns/sanitizable.rb
@@ -1,33 +1,31 @@
-module Users
-  module Sanitizable
-    extend ActiveSupport::Concern
+module Sanitizable
+  extend ActiveSupport::Concern
 
-    # NOT IN USE!
+  # NOT IN USE!
 
-    # add the following code to ApplicationRecord:
+  # add the following code to ApplicationRecord:
 
-    # include Sanitizable
+  # include Sanitizable
 
-    # ATTRIBUTES_TO_SANITIZE = []
-    # before_save -> { sanitize_attributes(ATTRIBUTES_TO_SANITIZE) }
+  # ATTRIBUTES_TO_SANITIZE = []
+  # before_save -> { sanitize_attributes(ATTRIBUTES_TO_SANITIZE) }
 
-    # and define ATTIRUBUTES_TO_SANITIZE for every model
+  # and define ATTIRUBUTES_TO_SANITIZE for every model
 
-    included do 
-      def sanitize(str)
-        ActionController::Base.helpers.sanitize(str)
-      end
+  included do 
+    def sanitize(str)
+      ActionController::Base.helpers.sanitize(str)
+    end
 
-      # accepts array of symbols
-      # updates attribute on instance
-      def sanitize_attributes(attributes)
-        attributes.each do |attribute|
-          next unless new_record? || will_save_change_to_attribute?(attribute)
+    # accepts array of symbols
+    # updates attribute on instance
+    def sanitize_attributes(attributes)
+      attributes.each do |attribute|
+        next unless new_record? || will_save_change_to_attribute?(attribute)
 
-          attribute = "@#{attribute}"
-          sanitized_str = sanitize(self.send(attribute))
-          self.instance_variable_set(attribute, sanitized_str)
-        end
+        attribute = "@#{attribute}"
+        sanitized_str = sanitize(self.send(attribute))
+        self.instance_variable_set(attribute, sanitized_str)
       end
     end
   end
