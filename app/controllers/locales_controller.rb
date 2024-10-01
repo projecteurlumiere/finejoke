1aab0fa9d55baf5cd3af882ff1070f22bef63e5d
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-10-01 20:57:25 +0300

    fix locales redirection and color-related styles

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -7,12 +7,30 @@ class LocalesController < ApplicationController
       current_or_guest_user.update(locale: params[:locale])
     end
 
-    redirect_to((params[:current_page] || root_path), params: { locale: I18n.locale }, status: :see_other)
+    path = set_redirect_path
+
+    redirect_to(path, status: :see_other)
   end
 
   private
 
   def no_authentication_required?
     true
+  end
+
+  def set_redirect_path
+    return root_path(params: { locale: I18n.locale }) unless params[:current_page]
+
+    url =  params[:current_page]
+    uri = URI.parse(url)
+
+    # parsing query into hash
+    query = Rack::Utils.parse_query(uri.query)
+
+    # Replace the value
+    query["locale"] = I18n.locale
+
+    uri.query = Rack::Utils.build_query(query)
+    uri.to_s
   end
 end
