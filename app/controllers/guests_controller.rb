84fae278c35ec7ec4bb8269def785dffdfb39746
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-10-06 14:11:51 +0300

    implement some authorization for guests_controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,8 @@
 class GuestsController < ApplicationController
   def create
     skip_authorization
+    raise Pundit::NotAuthorizedError if current_user
+
     session[:guest_welcomed] = true
     flash[:notice] = t(:".you_are_guest")
     redirect_to params[:referrer] === root_path ? games_path : params[:referrer]
