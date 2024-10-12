e902d05950a8a61ecf0384061a89a1ffc55c2f68
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-10-12 14:58:47 +0300

    fix locale-related errors when requesting setup for a new round

--- app/models/concerns/localizable.rb
+++ app/models/concerns/localizable.rb
@@ -5,7 +5,7 @@ module Localizable
     # NEVER change order - append only
     enum locale: I18n.available_locales
 
-    before_create :set_default_locale, unless: :locale_changed?
+    before_create :set_default_locale, unless: :locale_came_from_user?
 
     def set_default_locale
       raise "must be implemented in the extended class #{self.class}"
