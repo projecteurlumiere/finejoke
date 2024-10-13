e1d02714bb1a68614d01095f18103f2ddf8ece61
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-10-13 11:02:36 +0300

    fix errors when wrong locale supplied in params or no locale in header ..

--- app/controllers/concerns/locales_handling.rb
+++ app/controllers/concerns/locales_handling.rb
@@ -8,7 +8,7 @@ module LocalesHandling
       user = current_user || (!new_guest? && current_or_guest_user)
 
       # user can be false sometimes, not just nil
-      locale = params[:locale] ||
+      locale = extract_locale_from_params ||
                (user && user.locale) || 
                extract_locale_from_accept_language_header ||
                I18n.default_locale
@@ -16,13 +16,20 @@ module LocalesHandling
       I18n.with_locale(locale, &action)
     end
 
-    private 
+    private
+
+    def extract_locale_from_params
+      return unless params[:locale].present?
+
+      locale = params[:locale].to_sym
+      locale if I18n.locale_available?(locale)
+    end
 
     def extract_locale_from_accept_language_header
       lang_header = request.env["HTTP_ACCEPT_LANGUAGE"]
       return if lang_header.blank?
 
-      locale = lang_header.scan(/^[a-z]{2}/).first.to_sym
+      locale = lang_header.scan(/^[a-z]{2}/).first&.to_sym
       locale if I18n.locale_available?(locale)
     end
   end
