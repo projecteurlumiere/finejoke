3ba408db072c05ec4b60a984a2bf70ce99e180e5
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-10-14 13:46:16 +0300

    fix equality operator typo

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -7,7 +7,7 @@ class GuestsController < ApplicationController
     session[:guest_session_deadline] = Time.now + 1.day
     flash[:notice] = t(:".you_are_guest")
     current_or_guest_user.update_tracked_fields!(request)
-    redirect_to params[:referrer] === root_path ? games_path : params[:referrer]
+    redirect_to params[:referrer] == root_path ? games_path : params[:referrer]
   end
 
   private
