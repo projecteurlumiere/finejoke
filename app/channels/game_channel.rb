e2266a044df42bee622e7fc949969399da37e61b
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-10-17 12:55:28 +0300

    remove checking if user is in game in ws channel as it kicks viewers

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -2,9 +2,11 @@ class GameChannel < ApplicationCable::Channel
   # it is not possible to disconnect user from chanenl or stream from controller or model
   # therefore the channel periodically checks whether user is in game
   # to ensure he was not kicked and still has the right to receive game and game chat updates
-  periodically every: 1.minute do
-    stop_stream_for @game unless user_in_game?
-  end
+
+  # this is not necessary anymore because all games are always viewable
+  # periodically every: 1.minute do
+  #   stop_stream_for @game unless user_in_game?
+  # end
 
   def subscribed
     set_user
@@ -33,6 +35,7 @@ class GameChannel < ApplicationCable::Channel
     reject if @game.users.none?(@user)
   end
 
+  # this is not used anymore. see the top of the file 
   def user_in_game?
     User.where(id: @user.id).pluck(:game_id)[0] == @game.id
   end
