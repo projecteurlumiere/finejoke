ff252bd6747fe2285fdc06e2135a26613d490f52
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-10-18 15:56:49 +0300

    add nil check for round in CreateNewRoundJob

--- app/jobs/create_new_round_job.rb
+++ app/jobs/create_new_round_job.rb
@@ -4,7 +4,7 @@ class CreateNewRoundJob < ApplicationJob
   def perform(game_id, current_round_id)
     # Do something later
     current_round = Round.find_by(id: current_round_id)
-    return if current_round.passed?
+    return if current_round.nil? || current_round.passed?
     
     game = Game.find_by(id: game_id)
     return unless game.ongoing?
